0bf677beebee8a697f3adca054e64a6e01a8b302
Przemysław Piątek <p.piatek97@gmail.com>
2019-11-01 17:35:12 +0100

    Dodanie metod do obsługi kontrolera tworzenia nowego eksperymentu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 public class Main extends Application {
@@ -20,8 +21,133 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         launch(args);
-    }
 
+//
+//        final PipedOutputStream output = new PipedOutputStream();
+//        final PipedInputStream input = new PipedInputStream(output);
+
+
+//        Thread thread1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                ProcessBuilder builder = new ProcessBuilder("lbm.exe", "inout\\in\\ISBI2012\\addEllipse\\optionsMRI_addEllipse.txt", ">", "test.txt")
+//                        .directory(new File("E:\\Repozytoria\\inzynierka\\"));
+//                Process process = null;
+//                try {
+//                    process = builder.start();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+////                InputStream is = process.getInputStream();
+////                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+////                String line;
+////                try {
+////                    while ((line = reader.readLine()) != null) {
+////                        output.write(line.getBytes());
+////                    }
+////                } catch (IOException e) {
+////                    e.printStackTrace();
+////                }
+//
+//            }
+//        });
+//
+//        Thread thread2 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    int data = input.read();
+//                    while (data != -1) {
+//                        System.out.print((char) data);
+//                        data = input.read();
+//                    }
+//                } catch (IOException e) {
+//                }
+//            }
+//        });
+
+//        thread1.start();
+//        thread2.start();
+
+
+//        Thread thread1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                ProcessBuilder builder = new ProcessBuilder("lbm.exe", "inout\\in\\ISBI2012\\addEllipse\\optionsMRI_addEllipse.txt", "1>", "test.txt")
+//                        .directory(new File("E:\\Repozytoria\\inzynierka\\"))
+//                        .redirectInput(ProcessBuilder.Redirect.INHERIT)
+//                        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+//                        .redirectError(ProcessBuilder.Redirect.INHERIT);
+//                Process process = null;
+//                try {
+//                    process = builder.start();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+////                InputStream is = process.getInputStream();
+////                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+////                String line;
+////                try {
+////                    while ((line = reader.readLine()) != null) {
+////                        output.write(line.getBytes());
+////                    }
+////                } catch (IOException e) {
+////                    e.printStackTrace();
+////                }
+//
+//            }
+//        });
+//
+//
+//        thread1.start();
+
+
+//        Runtime rt = Runtime.getRuntime();
+//        Process proc = rt.exec("cmd /c E:\\Repozytoria\\inzynierka\\lbm.exe E:\\Repozytoria\\inzynierka\\inout\\in\\ISBI2012\\addEllipse\\optionsMRI_addEllipse.txt");
+//
+//        // Any error message?
+//        Thread errorGobbler
+//                = new Thread(new StreamGobbler(proc.getErrorStream(), System.err));
+//
+//        // Any output?
+//        Thread outputGobbler
+//                = new Thread(new StreamGobbler(proc.getInputStream(), System.out));
+//
+//        errorGobbler.start();
+//        outputGobbler.start();
+//
+//        // Any error?
+//        try {
+//            int exitVal = proc.waitFor();
+//            errorGobbler.join();   // Handle condition where the
+//            outputGobbler.join();  // process ends before the threads finish
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        try {
+//            Process process = new ProcessBuilder("start","/high","E:\\Repozytoria\\inzynierka\\lbm.exe", "E:\\Repozytoria\\inzynierka\\inout\\in\\ISBI2012\\bif\\optionsFLOWMRI_bif.txt")
+//                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+//                    .redirectInput(ProcessBuilder.Redirect.INHERIT)
+//                    .redirectError(ProcessBuilder.Redirect.INHERIT)
+//                    .start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        try {
+//            Process process = new ProcessBuilder("ping", "google.com", "-t")
+//                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+//                    .start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
 }
+
+
--- src/main/java/controllers/NewExperimentWindowController.java
+++ src/main/java/controllers/NewExperimentWindowController.java
@@ -1,4 +1,182 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 public class NewExperimentWindowController {
+    @FXML
+    private TextField newExperimentNumberOfTreesTextField;
+    @FXML
+    private TextField newExperimentMapSizeTextField;
+    @FXML
+    private TextField newExperimentMapSizeTreeOneTextField;
+    @FXML
+    private TextField newExperimentMapSizeTreeTwoTextField;
+    @FXML
+    private TextField newExperimentMapSizeTreeThreeTextField;
+    @FXML
+    private TextField newExperimentDimensionTextField;
+    @FXML
+    private TextField newExperimentNameTextField;
+    @FXML
+    private TextField newExperimentMRIObjectTextField;
+    @FXML
+    private TextField newExperimentMRIMethodTextField;
+    @FXML
+    private Button newExperimentMRIObjectFileButton;
+    @FXML
+    private TextField newExperimentMRIDimensionTextField;
+    @FXML
+    private TextField newExperimentTRTextField;
+    @FXML
+    private TextField newExperimentTETextField;
+    @FXML
+    private TextField newExperimentACQTimeTextField;
+    @FXML
+    private TextField newExperimentFATextField;
+    @FXML
+    private TextField newExperimentKindOfRfPulseTextField;
+    @FXML
+    private TextField newExperimentRfPulseTimeTextField;
+    @FXML
+    private TextField newExperimentRRfPulseNOfLobesTextField;
+    @FXML
+    private TextField newExperimentRfPulseBwTextField;
+    @FXML
+    private TextField newExperimentRfPulsePointsTextField;
+    @FXML
+    private TextField newExperimentRfSliceSelAxisTextField;
+    @FXML
+    private TextField newExperimentRfSliceSelThicknessTextField;
+    @FXML
+    private TextField newExperimentRfSliceSelFractionTextField;
+    @FXML
+    private TextField newExperimentRfSliceSelGradientTextField;
+    @FXML
+    private TextField newExperimentChangeRfPulseFlowStepTextField;
+    @FXML
+    private TextField newExperimentRfPulseFlowStepFactorTextField;
+    @FXML
+    private TextField newExperimentChangeBeforeACQFlowStepTextField;
+    @FXML
+    private TextField newExperimentBeforeACQFlowStepFactorTextField;
+    @FXML
+    private TextField newExperimentChangeAfterACQFlowStepTextField;
+    @FXML
+    private TextField newExperimentAfterACQFlowStepFactorTextField;
+    @FXML
+    private TextField newExperimentMRIFlowGmnXTextField;
+    @FXML
+    private TextField newExperimentMRIFlowGmnYTextField;
+    @FXML
+    private TextField newExperimentMRIFlowGmnZTextField;
+    @FXML
+    private TextField newExperimentMRIFlowGmnSliceSelTextField;
+    @FXML
+    private Button newExperimentCancelButton;
+    @FXML
+    private Button newExperimentSaveButton;
+    @FXML
+    private Button newExperimentDefaultValuesButton;
+    @FXML
+    private Button newExperimentReadFileWithOptionsButton;
+
+    private File mriObjectFileDesc;
+    private File mriObjectFileFlow;
+    private File mriObjectFileType;
+
+
+    @FXML
+    public void loadMRIObjectFileDesc(ActionEvent actionEvent) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("bundles.messages");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(resourceBundle.getString("new.experiment.mri.simulation.object.file.open.title"));
+        mriObjectFileDesc = fileChooser.showOpenDialog(null);
+    }
+
+    @FXML
+    public void loadMRIObjectFileFlow(ActionEvent actionEvent) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("bundles.messages");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(resourceBundle.getString("new.experiment.mri.simulation.object.file.open.title"));
+        mriObjectFileFlow = fileChooser.showOpenDialog(null);
+    }
+
+    @FXML
+    public void loadMRIObjectFileType(ActionEvent actionEvent) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("bundles.messages");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(resourceBundle.getString("new.experiment.mri.simulation.object.file.open.title"));
+        mriObjectFileFlow = fileChooser.showOpenDialog(null);
+    }
+
+    @FXML
+    void cancel(ActionEvent event) {
+        Stage stage = (Stage) newExperimentCancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void loadDefaultData(ActionEvent event) {
+        //Todo
+    }
+
+    @FXML
+    void readFileWithOptions(ActionEvent event) {
+        //Todo
+        ResourceBundle messagesBundle = ResourceBundle.getBundle("bundles.messages");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(messagesBundle.getString("new.experiment.load.file.with.options"));
+        File fileWithOptions = fileChooser.showOpenDialog(null);
+        try {
+            List<String> fileLines = Files.readAllLines(Paths.get(fileWithOptions.getPath()));
+            List<String> experimentParameters = getExperimentParametersFromBundle();
+            fileLines.forEach(line -> {
+                for (String experimentParameter : experimentParameters) {
+                    if (line.contains(experimentParameter)) {
+                        if (line.contains("#" + experimentParameter)) {
+                            //znaczy, że jest zakomentowane
+                        } else {
+                            //znaczy że nie zakomentowane -> bierzemy
+                        }
+                    }
+                }
+
+
+            });
+        } catch (IOException e) {
+            //Todo - pokazać jakiś message że błąd z plikiem
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void saveNewExperiment(ActionEvent event) {
+        //Todo
+    }
+
+    private List<String> getExperimentParametersFromBundle() {
+        List<String> experimentParameters = new LinkedList<>();
+        ResourceBundle parametersBunle = ResourceBundle.getBundle("bundles.experiment_parameters");
+        Enumeration<String> keys = parametersBunle.getKeys();
+        while (keys.hasMoreElements()) {
+            experimentParameters.add(parametersBunle.getString(keys.nextElement()));
+        }
+        return experimentParameters;
+    }
+
 }
+
